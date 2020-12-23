f53781907a558e7469294150e365f0f092564a13
Victor Valdez <victor334_@hotmail.com>
2020-12-23 09:11:27 -0400

    test 02 added to tests file

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,5 @@ export class Game {
     board: Array<Array<number>>
     constructor (board: Array<Array<number>>){
         this.board = board
- 
     }
 }
--- test/game.test.ts
+++ test/game.test.ts
@@ -14,3 +14,24 @@ describe ('test 01', () => {
 
     })
 })
+
+describe ('test 02', () => {
+    it('When theres one cell alive in the middle, and fewer than two live neighbours, it dies as if by underpopulation', function() {
+        const startingBoard = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+        ]
+
+        const nextStepBoard = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+
+        const game = new Game (startingBoard)
+        game.step()
+        expect (game.board).to.equal(nextStepBoard)
+
+    })
+})
